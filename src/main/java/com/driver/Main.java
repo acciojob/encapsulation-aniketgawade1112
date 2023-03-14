513a7880ad1d3e850136d01714a19df9dd2574c7
aniketgawade1112 <aniketgawade1112>
2023-03-14 19:26:11 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly r1 = new RWOnly();
+
+        r1.name = "Aniket";
+        System.out.println(name);
+        // Error - name has private access in com.driver.RWOnly
+
+        r1.setName("Aniket");
+        String name = r1.getName();
+
+        System.out.println(name);
+    }
 }
